9987e8346e82623b451cf0044f994ec9c7d7cb3e
Nick Collings <nick@collingo.com>
2014-10-21 17:09:28 +0100

    grunt watch setup, proxyquiring internal dependencies

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 'use strict';
-var proxyquire = require('proxyquire');
+var proxyquire = require('proxyquire').noCallThru();
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
@@ -14,17 +14,19 @@ describe('mapper', function() {
   var observer;
   var Mapper;
   var mapper;
+  var map;
 
   beforeEach(function() {
     sandbox = sinon.sandbox.create();
     store = {
       onChange: sandbox.spy()
     };
+    map = {};
     observer = sandbox.spy();
     Mapper = proxyquire('../src/mapper', {
       observer: observer
     });
-    mapper = new Mapper(store, {});
+    mapper = new Mapper(store, map);
   });
 
   afterEach(function() {
@@ -43,6 +45,7 @@ describe('mapper', function() {
 
     it('should bind to changes on the map', function() {
       expect(observer).to.be.have.been.calledOnce
+      expect(observer).to.be.have.been.calledWith(map)
     });
 
   });
