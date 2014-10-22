5d2c855e61fc0283753eb82e14b66e281872d489
Nick Collings <nick@collingo.com>
2014-10-22 13:21:51 +0100

    clean up and refactor

--- src/mapper.js
+++ src/mapper.js
@@ -12,38 +12,43 @@ function Mapper(store, map, updateable) {
 Mapper.prototype = {
   constructor: Mapper,
   getViewModel: function() {
-    return this.initMap().then(this.initDependants.bind(this));
+    return this.initMap()
+            .then(this.initDependants.bind(this))
+            .then(function() {
+              return this.viewModel;
+            }.bind(this));
   },
   initMap: function() {
     return Q.all(Object.keys(this.map).map(this.initMapping.bind(this)));
   },
-  initMapping: function(property) {
-    var mapping = this.map[property];
-    var deferred = Q.defer();
+  initMapping: function(path) {
+    var mapping = this.map[path];
     if(typeof mapping === 'string') {
-      this.store.once(property).then(function(property, value) {
-        this.setOnViewModel(property, value);
-        deferred.resolve();
-      }.bind(this, property));
+      return this.store.once(path).then(this.setOnViewModel.bind(this, path));
     } else {
-      var callback = mapping.pop();
-      mapping.forEach(this.registerDependency.bind(this, {
-        prop: property,
-        cb: callback,
-        deps: mapping
-      }));
-      deferred.resolve();
+      this.registerDependencies(path, mapping);
+      return Q();
     }
-    return deferred.promise;
   },
   getFromViewModel: function(path) {
     return this.viewModel[path];
   },
   setOnViewModel: function(path, value) {
     this.viewModel[path] = value;
+    this.updateDependantsFor(path);
+  },
+  updateDependantsFor: function(path) {
     if(this.dependencyMap[path]) {
       this.dependencyMap[path].forEach(this.setDependent.bind(this));
     }
+  },
+  registerDependencies: function(path, mapping) {
+    var callback = mapping.pop();
+    mapping.forEach(this.registerDependency.bind(this, {
+      prop: path,
+      cb: callback,
+      deps: mapping
+    }));
   },
   registerDependency: function(relationship, dependency) {
     if(!this.dependencyMap[dependency]) {
@@ -52,19 +57,10 @@ Mapper.prototype = {
     this.dependencyMap[dependency].push(relationship);
   },
   initDependants: function() {
-    var processed = [];
-    Object.keys(this.dependencyMap).forEach(this.processDependency.bind(this, processed));
-    return this.viewModel;
+    Object.keys(this.dependencyMap).forEach(this.processDependency.bind(this));
   },
-  processDependency: function(processed, dep) {
-    this.dependencyMap[dep].forEach(this.processDependencyRelationship.bind(this, processed));
-  },
-  processDependencyRelationship: function(processed, relationship) {
-    var sig = relationship.prop;
-    if(processed.indexOf(sig) < 0) {
-      this.setDependent(relationship);
-      processed.push(sig);
-    }
+  processDependency: function(dep) {
+    this.dependencyMap[dep].forEach(this.setDependent.bind(this));
   },
   setDependent: function(relationship) {
     this.setOnViewModel(relationship.prop, relationship.cb.apply(this, relationship.deps.map(this.getFromViewModel.bind(this))));
@@ -74,11 +70,6 @@ Mapper.prototype = {
   },
   onStoreChange: function(path, value) {
     this.setOnViewModel(path, value);
-  },
-  onViewModelChange: function(path, type, oldValue, newValue) {
-    if(this.dependencyMap[path]) {
-      this.dependencyMap[path].forEach(this.setDependent.bind(this));
-    }
   }
 };
 
